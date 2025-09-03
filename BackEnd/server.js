3862f783c21d53c57068caf43b52e25ec64be0fc
akshaysavra <savaraakshay2366@gmail.com>
2025-09-03 15:57:40 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -38,6 +38,13 @@ app.use(cors({
   },
   credentials: true
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://grocery-app-frontend-9dt5.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.get("/" ,(req,res)=>{
     res.send("hello from server")
